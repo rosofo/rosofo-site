aa06c24a9c86de60bd5c88942292be38e730e370
Robin Forbes <rsforbes0@gmail.com>
2023-03-24 02:12:25 +0000

    init

---
+++ src/nav.ts
@@ -0,0 +1,96 @@
+import { css, html, LitElement } from "lit";
+import { customElement, state } from "lit/decorators.js";
+
+@customElement("nav-breadcrumbs")
+export class NavBreadcrumbs extends LitElement {
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener("popstate", () => {
+      this.requestUpdate();
+    });
+  }
+
+  getCrumbsFromLocation() {
+    if (location.pathname === "/") {
+      return [];
+    }
+    const names = location.pathname.slice(1).split("/");
+    return names.map((name, index) => ({
+      name,
+      href: `/${names.slice(0, index + 1).join("/")}`,
+    }));
+  }
+  render() {
+    return html`
+      <nav>
+        <ul>
+          ${this.getCrumbsFromLocation().map(
+            (crumb) => html`<li><a href="${crumb.href}">${crumb.name}</a></li>`
+          )}
+        </ul>
+      </nav>
+    `;
+  }
+
+  static styles = css`
+    nav {
+      padding: 0 0.5rem;
+    }
+
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      list-style: none;
+      margin: 0;
+      padding: 0;
+    }
+
+    li:not(:last-child)::after,
+    li:first-child::before {
+      display: inline-block;
+      margin: 0 0.25rem;
+      font-family: "Averia Serif Libre", sans-serif;
+      content: var(--breadcrumb-sep, ">");
+    }
+
+    a {
+      text-decoration: none;
+      color: var(--magenta-haze, initial);
+      font-size: 1.2rem;
+    }
+
+    a:hover {
+      text-decoration: underline solid var(--magenta-haze) 0.2rem;
+    }
+  `;
+}
+
+@customElement("menu-button")
+export class MenuButton extends LitElement {
+  render() {
+    return html`<a class="desktop" href="/"><slot name="desktop-icon"></slot></a
+      ><button class="mobile"><slot name="mobile-icon"></slot></button>`;
+  }
+
+  static styles = css`
+    .desktop {
+      display: none;
+    }
+
+    @media only screen and (min-width: 768px) {
+      .desktop {
+        display: block;
+      }
+      .mobile {
+        display: none;
+      }
+    }
+
+    button {
+      border: none;
+      margin: 0;
+      padding: 0;
+      background: none;
+    }
+  `;
+}
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "android-chrome-384x384",
+      ],
+      manifest: {
+        name: "rosofo",
+        short_name: "rosofo",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      },
+    }),
+  ],
+});
