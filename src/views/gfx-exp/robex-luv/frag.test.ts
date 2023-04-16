47240cbceecd5e1efa2327e026e1e6309174ad4f
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 18:55:57 +0000

    16 Apr 2023, 19:55

--- src/views/gfx-exp/robex-luv/frag.test.ts
+++ src/views/gfx-exp/robex-luv/frag.test.ts
@@ -3,7 +3,7 @@ import { runFrag } from "../../../test-utils/frag";
 import glsl from "glslify";
 
 test("isBlock can be used to set a single value", async () => {
-  const frag = await import("./frag.glsl?url");
+  const frag = (await import("./frag.glsl?url")).default;
   const result = glsl`
   #pragma glslify: isBlock = require('${frag}')
   `;
