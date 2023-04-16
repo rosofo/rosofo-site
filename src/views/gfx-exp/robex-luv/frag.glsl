a4f05c1fb29905f28251fcb13447513575b1201f
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 02:44:55 +0000

    16 Apr 2023, 03:44

--- src/views/gfx-exp/robex-luv/frag.glsl
+++ src/views/gfx-exp/robex-luv/frag.glsl
@@ -5,7 +5,7 @@ uniform int u_height;
 
 void main() {
     vec4 data = texture2D(u_texture, gl_FragCoord.xy);
-    bool is_a = gl_FragCoord.x / float(u_width) < 0.01f && gl_FragCoord.y / float(u_height) < 0.01f;
+    bool is_a = gl_FragCoord.x < 1.0f && gl_FragCoord.y < 1.0f;
     
     vec4 new_a = float(is_a) * (data + 0.01f);
     vec4 curr_a = texture2D(u_texture, vec2(0.0f, 0.0f));
