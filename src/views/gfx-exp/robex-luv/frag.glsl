0fecc4830fce9a6ea323cccff3f36350dbfce306
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 02:13:19 +0000

    16 Apr 2023, 03:13

--- src/views/gfx-exp/robex-luv/frag.glsl
+++ src/views/gfx-exp/robex-luv/frag.glsl
@@ -3,13 +3,13 @@ uniform int u_width;
 uniform int u_height;
 
 vec2 toIntCoord(vec2 fragCoord) {
-    vec2 intCoord = (fragCoord * vec2(float(u_width), float(u_height))) - 0.5f;
+    vec2 intCoord = (fragCoord * vec2(float(u_width), float(u_height)));
     return vec2(int(intCoord.x), int(intCoord.y));
 }
 
 void main() {
     vec4 data = texture2D(u_texture, gl_FragCoord.xy);
-    bool is_a = toIntCoord(gl_FragCoord.xy) == vec2(1, 0);
+    bool is_a = toIntCoord(gl_FragCoord.xy) == vec2(0, 0);
     
     vec4 new_a = float(is_a) * (data + 0.01f);
     vec4 curr_a = texture2D(u_texture, vec2(0.0f, 0.0f));
