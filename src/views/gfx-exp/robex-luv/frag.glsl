902d8a2ffbc873df6693338d21f862441340c271
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 01:56:40 +0000

    16 Apr 2023, 02:56

--- src/views/gfx-exp/robex-luv/frag.glsl
+++ src/views/gfx-exp/robex-luv/frag.glsl
@@ -8,7 +8,7 @@ vec2 toIntCoord(vec2 fragCoord) {
 
 void main() {
     vec4 data = texture2D(u_texture, gl_FragCoord.xy);
-    bool is_a = toIntCoord(gl_FragCoord.xy) == vec2(0, 0);
+    bool is_a = gl_FragCoord.xy == vec2(0.0f, 0.0f);
     
     vec4 new_a = float(is_a) * (data + 0.01f);
     
