917d3ad27855157018dc63125ec24b23beee1b8b
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 02:37:40 +0000

    16 Apr 2023, 03:37

--- src/views/gfx-exp/robex-luv/frag.glsl
+++ src/views/gfx-exp/robex-luv/frag.glsl
@@ -2,20 +2,14 @@ uniform sampler2D u_texture;
 uniform int u_width;
 uniform int u_height;
 
-layout(origin_upper_left, pixel_center_integer) in vec4 gl_FragCoord;
-
-vec2 toIntCoord(vec2 fragCoord) {
-    vec2 intCoord = (fragCoord * vec2(float(u_width), float(u_height)));
-    return vec2(int(intCoord.x), int(intCoord.y));
-}
 
 void main() {
     vec4 data = texture2D(u_texture, gl_FragCoord.xy);
-    bool is_a = ivec2(gl_FragCoord.xy) == ivec2(0, 0);
+    bool is_a = gl_FragCoord.x < 0.1f && gl_FragCoord.y < 0.1f;
     
     vec4 new_a = float(is_a) * (data + 0.01f);
     vec4 curr_a = texture2D(u_texture, vec2(0.0f, 0.0f));
-    vec4 new_rest = float(!is_a) * vec4(curr_a.x, 0.0f, 0.0f, 1.0f);
-    
-    gl_FragColor = float(!is_a) * vec4(1.0f, 0.0f, 0.0f, 1.0f) + new_a;
+    vec4 new_rest = float(!is_a) * vec4(curr_a.x);
+
+    gl_FragColor = new_a + new_rest;
 }
