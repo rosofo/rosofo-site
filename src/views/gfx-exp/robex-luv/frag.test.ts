209b08cec833483aca0d95071ffeb50697c3fa99
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 20:35:03 +0000

    16 Apr 2023, 21:35

--- src/views/gfx-exp/robex-luv/frag.test.ts
+++ src/views/gfx-exp/robex-luv/frag.test.ts
@@ -1,8 +1,22 @@
+import * as THREE from "three";
 import { expect, test } from "vitest";
 import { extractGlslFunction, runFrag } from "../../../test-utils/frag";
 import frag from "./frag.glsl?raw";
 
 test("isBlock can be used to set a single value", async () => {
   const isBlock = extractGlslFunction(frag, "isBlock");
-  expect(isBlock);
+  const testFrag = `
+  uniform sampler2D u_texture;
+  
+  ${isBlock}
+  
+  void main() {
+    vec4 data = texture2D(u_texture, gl_FragCoord.xy);
+    return float(isBlock(gl_FragCoord, 0)) * vec4(1.0f);
+  }
+  `;
+  const data = new Uint8Array(8);
+  const result = runFrag(testFrag, data, 2, 1);
+
+  expect(result);
 });
