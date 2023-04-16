7c7bcb6de75d5dca5b5e4557b697f6342c68f3a1
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 14:58:03 +0000

    16 Apr 2023, 15:58

--- src/views/gfx-exp/robex-luv/frag.glsl
+++ src/views/gfx-exp/robex-luv/frag.glsl
@@ -3,6 +3,7 @@ uniform int u_width;
 uniform int u_height;
 
 float blockSize = 1.0f;
+int blocks = 1;
 
 bool isBlock(vec2 coord, int index) {
     float min = blockSize * float(index);
@@ -10,15 +11,41 @@ bool isBlock(vec2 coord, int index) {
     return coord.x >= min && coord.x <= max && coord.y <= 1.0f;
 }
 
+vec4 getBlock(int index) {
+    float x = blockSize * float(index) + (blockSize * 0.5f);
+    return texture2D(u_texture, vec2(x, blockSize * 0.5f));
+}
+
+vec4 getA() {
+    return getBlock(0);
+}
+
+float getLove() {
+    vec4 a = getA();
+    return floor(a.x * 3.0f);
+}
+
+vec4 setBlock(int index, vec4 value) {
+    return float(isBlock(gl_FragCoord.xy, index)) * value;
+}
+
+vec4 setPixel(vec4 value) {
+    return float(gl_FragCoord.y > 1.0f || gl_FragCoord.x > float(blocks) * blockSize) * value;
+}
+
 void main() {
     vec4 data = texture2D(u_texture, gl_FragCoord.xy);
-    bool is_a = isBlock(gl_FragCoord.xy, 0);
     
-    vec4 new_a = float(is_a) * (data + 0.01f);
-    vec4 curr_a = texture2D(u_texture, vec2(0.0f, 0.0f));
+    #if DEBUG
+    vec4 outDebugColor;
+    #else
     
-    float love = curr_a.x * 100.0f;
-    vec4 new_rest = float(!is_a) * vec4(curr_a.x, 0.0f, 0.0f, 1.0f);
-
-    gl_FragColor = new_a + new_rest;
+    #if DEBUG
+    outDebugColor = vec4(0.5f) * float(isBlock(gl_FragCoord.xy, 0));
+    #else
+    
+    #if DEBUG
+    gl_FragColor = outDebugColor;
+    #else
+    #endif
 }
