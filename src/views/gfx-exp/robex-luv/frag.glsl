fc69301c249bf50e2943756af699df4712f1e544
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 02:08:00 +0000

    16 Apr 2023, 03:08

--- src/views/gfx-exp/robex-luv/frag.glsl
+++ src/views/gfx-exp/robex-luv/frag.glsl
@@ -12,6 +12,8 @@ void main() {
     bool is_a = toIntCoord(gl_FragCoord.xy) == vec2(0, 0);
     
     vec4 new_a = float(is_a) * (data + 0.01f);
+    vec4 curr_a = texture2D(u_texture, vec2(0.0f, 0.0f));
+    vec4 new_rest = float(!is_a) * vec4(curr_a.x, 0.0f, 0.0f, 1.0f);
     
-    gl_FragColor = float(!is_a) * texture2D(u_texture, vec2(0.0f, 0.0f)) + new_a;
+    gl_FragColor = new_rest + new_a;
 }
