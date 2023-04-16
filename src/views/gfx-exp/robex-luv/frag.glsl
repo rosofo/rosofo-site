a12957ab82d396714df84777cfee6dbc9b4675dd
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 13:46:35 +0000

    16 Apr 2023, 14:46

--- src/views/gfx-exp/robex-luv/frag.glsl
+++ src/views/gfx-exp/robex-luv/frag.glsl
@@ -2,10 +2,17 @@ uniform sampler2D u_texture;
 uniform int u_width;
 uniform int u_height;
 
+float blockSize = 1.0f;
+
+bool isBlock(vec2 coord, int index) {
+    float min = blockSize * float(index);
+    float max = blockSize * (float(index) + 1.0f);
+    return coord.x >= min && coord.x <= max && coord.y <= 1.0f;
+}
 
 void main() {
     vec4 data = texture2D(u_texture, gl_FragCoord.xy);
-    bool is_a = gl_FragCoord.x < 1.0f && gl_FragCoord.y < 1.0f;
+    bool is_a = isBlock(gl_FragCoord.xy, 0);
     
     vec4 new_a = float(is_a) * (data + 0.01f);
     vec4 curr_a = texture2D(u_texture, vec2(0.0f, 0.0f));
