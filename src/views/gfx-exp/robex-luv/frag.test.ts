380a8cd124d73974d73bcd60a902238ef56e2e86
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 18:45:28 +0000

    16 Apr 2023, 19:45

--- src/views/gfx-exp/robex-luv/frag.test.ts
+++ src/views/gfx-exp/robex-luv/frag.test.ts
@@ -1,7 +1,11 @@
-import { test } from "vitest";
+import { expect, test } from "vitest";
 import { runFrag } from "../../../test-utils/frag";
 import glsl from "glslify";
+import frag from "./frag.glsl?url";
 
 test("isBlock can be used to set a single value", () => {
-  runFrag(glsl);
+  const result = glsl`
+  #pragma glslify: isBlock = require('${frag}')
+  `;
+  expect(result);
 });
