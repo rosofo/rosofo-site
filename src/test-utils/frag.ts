eef7c01cc4857e2e4325c10f1383aa613ef56e4b
Robin Forbes <rsforbes0@gmail.com>
2023-04-16 16:12:36 +0000

    16 Apr 2023, 17:12

--- src/test-utils/frag.ts
+++ src/test-utils/frag.ts
@@ -1,16 +1,29 @@
 import createContext from "gl";
 import * as THREE from "three";
 
-function testFrag(frag: string, dataWidth: number, dataHeight: number) {
+export function runFrag(
+  frag: string,
+  data: Uint8Array,
+  dataWidth: number,
+  dataHeight: number
+) {
   const context = createContext(100, 100);
   const renderer = new THREE.WebGLRenderer({ context });
-  const texture = new THREE.DataTexture(
-    new Uint8Array(dataWidth * dataHeight),
-    dataWidth,
-    dataHeight
-  );
+  const texture = new THREE.DataTexture(data, dataWidth, dataHeight);
+  const target = new THREE.WebGLRenderTarget(dataWidth, dataHeight);
   const material = new THREE.ShaderMaterial({
     fragmentShader: frag,
     uniforms: { u_texture: { value: texture } },
   });
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(80, 1);
+  const plane = new THREE.Mesh(new THREE.PlaneGeometry(), material);
+  scene.add(plane);
+  camera.position.z = 0.6;
+
+  renderer.setRenderTarget(target);
+  renderer.render(scene, camera);
+
+  return target.texture.source;
 }
