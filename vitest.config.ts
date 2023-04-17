3d078f213bcb8221a9508f8becf7b3d0fb9c0713
Robin Forbes <rsforbes0@gmail.com>
2023-04-17 23:54:07 +0000

    18 Apr 2023, 00:54

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     browser: {
+      enabled: true,
       provider: "playwright",
       name: "chromium",
     },
+    include: ["**/*.webtest.ts"],
   },
 });
