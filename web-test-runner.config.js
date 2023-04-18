4185a76c528898c5f80762e1413cf16c31836c96
Robin Forbes <rsforbes0@gmail.com>
2023-04-18 23:29:29 +0000

    19 Apr 2023, 00:29

--- web-test-runner.config.js
+++
@@ -1,7 +0,0 @@
-import { esbuildPlugin } from '@web/dev-server-esbuild';
-
-export default {
-  files: ['src/**/*.web.test.ts'],
-  plugins: [esbuildPlugin({ ts: true })],
-    nodeResolve: true
-};
