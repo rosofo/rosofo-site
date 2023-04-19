e0d8867ab4324231f8dd82024786117be33bdc05
Robin Forbes <rsforbes0@gmail.com>
2023-04-19 23:12:33 +0000

    20 Apr 2023, 00:12

--- test-utils/browser/index.ts
+++ test-utils/browser/index.ts
@@ -1,6 +1,9 @@
 import { createServer } from "vite";
 import { expect } from "vitest";
 import { chromium } from "playwright";
+import { exec } from "child_process";
+
+async function serve(filepath: string) {}
 
 export async function runInBrowser(filepath: string) {
   const browser = await chromium.launch();
