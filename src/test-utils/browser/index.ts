29aef9fceac7b807541551adce3cd5bcda5adffe
Robin Forbes <rsforbes0@gmail.com>
2023-04-19 23:37:04 +0000

    20 Apr 2023, 00:37

--- src/test-utils/browser/index.ts
+++ src/test-utils/browser/index.ts
@@ -1,12 +1,12 @@
 import { createServer } from "vite";
 import { expect } from "vitest";
 import { chromium } from "playwright";
-import { exec } from "child_process";
+import { spawn } from "child_process";
 import { resolve } from "path";
 
 async function serve(filepath: string) {
   process.env.VITE_BROWSER_SCRIPT = filepath;
-  return exec(`npx vite --port 5000 ${resolve(__dirname, "index.html")}`);
+  return spawn(`npx vite --port 5000 ${resolve(__dirname, "index.html")}`);
 }
 
 export async function runInBrowser(filepath: string) {
