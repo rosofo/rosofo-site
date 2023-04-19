79e047bf01aae5ad64021b199c42d80da93d79e9
Robin Forbes <rsforbes0@gmail.com>
2023-04-19 23:10:59 +0000

    20 Apr 2023, 00:10

--- test-utils/browser/index.ts
+++ test-utils/browser/index.ts
@@ -4,9 +4,6 @@ import { chromium } from "playwright";
 
 export async function runInBrowser(filepath: string) {
   const browser = await chromium.launch();
-  console.log(browser);
   const page = await browser.newPage();
-  console.log(page);
-  console.log(await page.title());
   await page.goto("http://localhost:5000");
 }
