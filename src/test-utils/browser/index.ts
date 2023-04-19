00b30142469d9fd79edbed93dc8262d3048575be
Robin Forbes <rsforbes0@gmail.com>
2023-04-19 23:35:05 +0000

    20 Apr 2023, 00:35

--- src/test-utils/browser/index.ts
+++ src/test-utils/browser/index.ts
@@ -10,6 +10,8 @@ async function serve(filepath: string) {
 }
 
 export async function runInBrowser(filepath: string) {
+  serve(filepath);
+
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto("http://localhost:5000");
