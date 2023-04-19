780b42cd16da11aaeb43917da874b8df071b2d7d
Robin Forbes <rsforbes0@gmail.com>
2023-04-19 23:19:09 +0000

    20 Apr 2023, 00:19

--- test-utils/browser/index.ts
+++ test-utils/browser/index.ts
@@ -2,8 +2,11 @@ import { createServer } from "vite";
 import { expect } from "vitest";
 import { chromium } from "playwright";
 import { exec } from "child_process";
+import { resolve } from "path";
 
-async function serve(filepath: string) {}
+async function serve(filepath: string) {
+  const process = exec(`npx vite ${resolve(__dirname, "index.html")}`);
+}
 
 export async function runInBrowser(filepath: string) {
   const browser = await chromium.launch();
