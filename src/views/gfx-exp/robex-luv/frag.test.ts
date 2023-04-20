025755f6386c720031ba151fecdc4a2cd330c27f
Robin Forbes <rsforbes0@gmail.com>
2023-04-20 20:37:59 +0000

    20 Apr 2023, 21:37

--- src/views/gfx-exp/robex-luv/frag.test.ts
+++ src/views/gfx-exp/robex-luv/frag.test.ts
@@ -1,33 +1,6 @@
-import { FragDebugConfig, FragDebugger } from "test-utils/frag";
-
-import { LitElement, html, css } from "lit";
-import { customElement, property } from "lit/decorators.js";
-import { ref } from "lit/directives/ref.js";
+import { FragTest, FragDebugConfig } from "test-utils/frag";
 import { test, expect } from "@sand4rt/experimental-ct-web";
 import frag from "./frag.glsl?raw";
-
-@customElement("frag-test")
-export class FragTest extends LitElement {
-  @property({ type: Object })
-  config!: FragDebugConfig;
-
-  debugger = new FragDebugger(this, this.config);
-  static styles = [
-    css`
-      :host {
-        display: block;
-      }
-    `,
-  ];
-
-  render() {
-    return html`
-      <canvas ${ref(this.debugger.ref)}></canvas>
-      <p>${this.config.initData}</p>
-      <p>${this.debugger.resultData}</p>
-    `;
-  }
-}
 
 test("it sets pixel 0,0 only", async ({ mount }) => {
   const component = await mount(FragTest, {
