a47e9f78fff7d135dacfeae709c232dd6836bdc9
Robin Forbes <rsforbes0@gmail.com>
2023-04-20 21:58:15 +0000

    20 Apr 2023, 22:58

--- src/views/gfx-exp/robex-luv/glsl-functions.ts
+++ src/views/gfx-exp/robex-luv/glsl-functions.ts
@@ -1,6 +1,11 @@
 import { extractGlslFunction } from "../../../test-utils/frag.ts";
 import { readFile } from "fs/promises";
-import { resolve } from "path";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
 
 export const isBlock = extractGlslFunction(
   await readFile(resolve(__dirname, "./frag.glsl"), { encoding: "utf-8" }),
