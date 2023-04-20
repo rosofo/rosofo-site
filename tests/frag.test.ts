6627124da9321722509dd0db6bec3bdb65d2aa5d
Robin Forbes <rsforbes0@gmail.com>
2023-04-20 23:34:24 +0000

    21 Apr 2023, 00:34

--- tests/frag.test.ts
+++ tests/frag.test.ts
@@ -3,13 +3,20 @@ import { test, expect } from "@sand4rt/experimental-ct-web";
 import { isBlock } from "../src/views/gfx-exp/robex-luv/glsl-functions.ts";
 
 test("isBlock sets pixel 0,0 only", async ({ mount }) => {
+  const frag = `
+  ${isBlock}
+  
+  void main() {
+    gl_FragColor = vec4(1.0f) * float(isBlock(gl_FragCoord.xy, 0));
+  }
+  `;
   const component = await mount(FragTest, {
     props: {
       config: {
         initData: new Uint8Array(16),
         dataWidth: 2,
         dataHeight: 2,
-        frag: isBlock,
+        frag,
       },
     } as any,
   });
