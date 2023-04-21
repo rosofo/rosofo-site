0684fa5da0e5f2c53a990ee73fd612bd99dc7569
Robin Forbes <rsforbes0@gmail.com>
2023-04-21 22:09:45 +0000

    21 Apr 2023, 23:09

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  build: {
-    target: "esnext",
-  },
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
