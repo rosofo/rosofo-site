939f13d2d83d5ac07d7046c0a829418a0a710353
Robin Forbes <rsforbes0@gmail.com>
2023-05-19 22:54:47 +0000

    19 May 2023, 23:54

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -14,14 +14,22 @@ vec4 setVar(int index, vec4 color) {
     return setTexel(ivec2(index, 0), color);
 }
 
+vec4 getVar(int index) {
+    return texelFetch(u_texture, ivec2(index, 0), 0);
+}
+
+
 void main() {
     ivec2 texCoord = ivec2(gl_FragCoord.x / 3.0f, gl_FragCoord.y / 2.4f);
     vec4 color = vec4(length(vec2(texCoord)) / 600.0);
     color.w = 1.0;
     
-    vec4 a = setVar(0, vec4(1.0));
-    vec4 b = setVar(1, vec4(0.9));
-    vec4 c = setVar(2, vec4(0.8));
-    vec4 d = setVar(3, vec4(0.7));
-    gl_FragColor = color + a + b + c + d;
+    bool isStart = !bool(getVar(0).w);
+    vec4 newIsStart = vec4(0.0, 0.0, 0.0, 1.0);
+    
+    gl_FragColor = color + newIsStart;
+}
+
+float rand(vec2 co) {
+    return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
 }
