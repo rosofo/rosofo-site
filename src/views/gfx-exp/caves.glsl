ef8f5ecc23bafb53e5b3b946f68f224d5db3a8c6
Robin Forbes <rsforbes0@gmail.com>
2023-05-19 22:19:10 +0000

    19 May 2023, 23:19

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -11,8 +11,13 @@ vec4 setTexel(ivec2 coord, vec4 color) {
 }
 
 void main() {
+    vec4 debug;
     vec4 curr_tX = texelFetch(u_texture, ivec2(0, 0), 0);
-    vec4 new_tX = setTexel(ivec2(0, 0), vec4(mod(curr_tX.x + 0.0000005, 250.0)));
-    vec4 current =  curr_tX * float(!texelActive(ivec2(0, 0)));
-    gl_FragColor = new_tX + current;
+    vec4 new_tX = setTexel(ivec2(0, 0), vec4(mod(curr_tX.x + 0.01, 250.0)));
+
+    if (curr_tX.x > 0.2) {
+        debug = vec4(1.0);
+    }
+    
+    gl_FragColor = new_tX + debug;
 }
