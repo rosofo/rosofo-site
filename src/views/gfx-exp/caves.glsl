c284508fc2ca1ae15ded321433914e5c547ae4d4
Robin Forbes <rsforbes0@gmail.com>
2023-05-19 20:29:55 +0000

    19 May 2023, 21:29

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -1,6 +1,14 @@
 uniform sampler2D u_texture;
 uniform float u_time;
 
+bool isTexel(ivec2 coord) {
+    return ivec2(gl_FragCoord.xy) == coord;
+}
+
+vec4 setTexel(ivec2 coord, vec4 color) {
+    return color * isTexel(coord);
+}
+
 void main() {
     vec4 color = texelFetch(u_texture, ivec2(gl_FragCoord.xy), 0);
     gl_FragColor = color;
