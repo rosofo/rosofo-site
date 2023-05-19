f715e92e1f467d7e9feb3ab7e59bcbc4467865ee
Robin Forbes <rsforbes0@gmail.com>
2023-05-19 20:10:01 +0000

    19 May 2023, 21:10

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -2,9 +2,6 @@ uniform sampler2D u_texture;
 uniform float u_time;
 
 void main() {
-    vec4 color = texture2D(u_texture, gl_FragCoord.xy);
-    color.z = (2.0f + cos(gl_FragCoord.x * u_time) + sin(gl_FragCoord.y * (1.0f - u_time))) * 0.5f;
-    color.x = (1.0f + sin(float((int(color.z * 100.0f) % 6)))) * 0.5f;
-    color.z = pow(color.x, color.z);
+    vec4 color = texelFetch(u_texture, ivec2(gl_FragCoord.xy), 0);
     gl_FragColor = color;
 }
