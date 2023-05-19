079848a4bc637e4d1fffc30719bad07885841b7c
Robin Forbes <rsforbes0@gmail.com>
2023-05-19 17:58:51 +0000

    19 May 2023, 18:58

--- src/router.ts
+++ src/router.ts
@@ -12,48 +12,7 @@ export const routingState = createStore<{
   };
 });
 
-const viewModules = import.meta.glob("./**/views/**/*.ts");
-type ViewHierarchy = (string | [string, ViewHierarchy])[];
-const viewHierarchy: ViewHierarchy = [
-  [
-    "/",
-    [
-      "stress-waves",
-      "hash-trees",
-      "pure-d3",
-      "maro-market",
-      ["gfx-exp", ["robex-luv", "coord-sines"]],
-    ],
-  ],
-];
-
-const findViews = (hierarchy: ViewHierarchy): Route[] => {
-  return hierarchy.map((entry) => {
-    let name: string;
-    let subViews;
-    if (typeof entry === "string") {
-      name = entry;
-    } else {
-      name = entry[0];
-      subViews = entry[1];
-    }
-
-    const result = Object.entries(viewModules).find(([path]) =>
-      path.endsWith(name)
-    );
-    const module = result ? result[1] : undefined;
-    return {
-      path: name,
-      component: module && name,
-      name,
-      action: module && (async () => await module()),
-      children: subViews ? findViews(subViews) : undefined,
-    };
-  });
-};
-
 export const router = new VRouter(document.getElementById("router-outlet"));
-export const views: Route[] = findViews(viewHierarchy);
 router.setRoutes([
   ...views,
   {
