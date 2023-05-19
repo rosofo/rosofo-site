7afe4b068ac36744fa9ad7d4ea273bb00d74202e
Robin Forbes <rsforbes0@gmail.com>
2023-05-19 23:10:24 +0000

    20 May 2023, 00:10

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -37,5 +37,5 @@ void main() {
     
     vec4 randomColorStart = cond(!varActive(), cond(isStart, vec4(vec3(rand(gl_FragCoord.xy)), 1.0)));
     
-    gl_FragColor = newIsStart + randomColorStart;
+    gl_FragColor = (newIsStart + randomColorStart) * cond(getVar(0).w < 1.0 && getVar(0).w > 0.0, 0.0);
 }
