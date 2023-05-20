6f282d8b5f0d1294e0b1eee99d06e8f530fcdcc0
Robin Forbes <rsforbes0@gmail.com>
2023-05-20 21:25:48 +0000

    20 May 2023, 22:25

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -33,6 +33,10 @@ vec4 set(int index, vec4 value) {
     return setTexel(ivec2(index, 0), value);
 }
 
+vec4 debugColor(int index, vec4 value) {
+    return gl_FragCoord.x >= float(index) && gl_FragCoord.x < float(index + 1) ? value : vec4(0.0);
+}
+
 void main() {
     ivec2 size = textureSize(u_texture, 0);
     
@@ -41,6 +45,8 @@ void main() {
     
     if (texelCoord(gl_FragCoord).y > 10) {
         gl_FragColor = vec4((x.xyz / 2.0), 1.0);
+    } else if (texelCoord(gl_FragCoord.y) > 0) {
+        gl_FragColor = debugColor(x);
     } else {
         gl_FragColor = newX + set(201, vec4(x.x > 100.0 ? 1.0 : 0.0, x.x > 10.0 ? 1.0 : 0.0, 0.0, 1.0));
     }
