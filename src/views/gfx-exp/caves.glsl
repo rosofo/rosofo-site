703dffe398a7ce599662a5f4b55713f137b3fe06
Robin Forbes <rsforbes0@gmail.com>
2023-05-20 23:20:34 +0000

    21 May 2023, 00:20

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -3,6 +3,8 @@ precision highp float;
 uniform sampler2D u_texture;
 uniform float u_x;
 uniform float u_time;
+uniform vec2 u_p1;
+uniform vec2 u_p2;
 
 float rand(vec2 co) {
     return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
@@ -37,10 +39,6 @@ vec4 debugColor(int index, vec4 value) {
     return gl_FragCoord.x >= findex && gl_FragCoord.x < findex + 8.0 ? value : vec4(0.0);
 }
 
-ivec2 spawn(float t) {
-    return ivec2(rand(vec2(sin(t), t)) * 200.0, rand(vec2(t, cos(t))) * 200.0);
-}
-
 void main() {
     ivec2 size = textureSize(u_texture, 0);
 
@@ -50,15 +48,11 @@ void main() {
     vec4 started = get(201);
     vec4 newStart = started.x == 0.0 ? set(201, vec4(1.0)) : set(201, started);
 
-    vec4 newfa = set(202, vec4(spawn(u_time).xy, 0.0, 1.0));
-    vec4 newfb = set(203, vec4(spawn(u_time).xy, 0.0, 1.0));
-
-    ivec2 fa = ivec2(get(202).xy);
     ivec2 sc = texelCoord(gl_FragCoord);
-    vec4 lineColor = vec4(length(vec2(fa - sc)) / float(size.x));
+    vec4 lineColor = vec4(length(vec2(u_p1 - sc)) / float(size.x));
 
     if(texelCoord(gl_FragCoord).y > 10) {
-        gl_FragColor = lineColor + setTexel(fa, vec4(1.0, 0.0, 0.0, 1.0));
+        gl_FragColor = lineColor + setTexel(fa, vec4(1.0f, 0.0f, 0.0f, 1.0f));
     } else if(texelCoord(gl_FragCoord).y > 0) {
         gl_FragColor = debugColor(0, x) + debugColor(1, started);
     } else {
