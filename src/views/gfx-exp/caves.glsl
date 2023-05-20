a72ae1646e435a354fbb72dc0bf6fde4af3bb7ed
Robin Forbes <rsforbes0@gmail.com>
2023-05-20 00:15:26 +0000

    20 May 2023, 01:15

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -22,13 +22,13 @@ vec4 getVar(int index) {
     return texelFetch(u_texture, ivec2(index, 0), 0);
 }
 
-#define COND_TEMPLATE(type) \
-    type cond(bool p, type v) { \
+#define _IF_TEMPLATE(type) \
+    type _if(bool p, type v) { \
         return float(p) * v; \
     }
 
-COND_TEMPLATE(float)
-COND_TEMPLATE(vec4)
+_IF_TEMPLATE(float)
+_IF_TEMPLATE(vec4)
 
 bool varActive() {
     ivec2 c = ivec2(gl_FragCoord.x / 3.0f, gl_FragCoord.y / 2.4f);
@@ -39,8 +39,8 @@ void main() {
     bool isStart = getVar(0).w == 1.0;
     vec4 newIsStart = vec4(0.0);
     
-    vec4 randomColorStart = cond(!varActive(), cond(isStart, vec4(vec3(rand(gl_FragCoord.xy)), 1.0)));
+    vec4 randomColorStart = _if(!varActive(), _if(isStart, vec4(vec3(rand(gl_FragCoord.xy)), 1.0)));
     
     bool invalidStart = getVar(0).w < 1.0 && getVar(0).w > 0.0;
-    gl_FragColor = (newIsStart + randomColorStart) * cond(invalidStart, 1.0);
+    gl_FragColor = (newIsStart + randomColorStart) * _if(invalidStart, 1.0);
 }
