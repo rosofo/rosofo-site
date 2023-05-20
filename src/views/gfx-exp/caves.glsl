1c62e33a3e8245171e07c2c2953106cbdeb4742a
Robin Forbes <rsforbes0@gmail.com>
2023-05-20 15:07:29 +0000

    20 May 2023, 16:07

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -19,42 +19,6 @@ vec4 getTexel(vec4 coord) {
     return texelFetch(u_texture, ivec2(coord.x / 3.0f, coord.y / 2.4f), 0);
 }
 
-vec4 getCurrentTexel() {
-    return texelFetch(u_texture, ivec2(gl_FragCoord.x / 3.0f, gl_FragCoord.y / 2.4f), 0);
-}
-
-vec4 setVar(int index, vec4 color) {
-    return setTexel(ivec2(index, 0), color);
-}
-
-vec4 getVar(int index) {
-    return texelFetch(u_texture, ivec2(index, 0), 0);
-}
-
-#define _IF_TEMPLATE(type) \
-    type _if(bool p, type v) { \
-        return float(p) * v; \
-    }
-
-_IF_TEMPLATE(float)
-_IF_TEMPLATE(vec4)
-
-bool varActive() {
-    ivec2 c = ivec2(gl_FragCoord.x / 3.0f, gl_FragCoord.y / 2.4f);
-    return c.y == 0;
-}
-
-vec4 pickRandTexel() {
-    return texelFetch(u_texture, ivec2(rand(gl_FragCoord.xy) * 200.0, rand(gl_FragCoord.xy) * 200.0), 0);
-}
-
-vec4 saveColor(vec4 color) {
-    return _if(!varActive(), color);
-}
-
 void main() {
-    vec4 a = getVar(100);
-    vec4 b = getVar(102);
-    
-    gl_FragColor = setVar(100, u_a) + setVar(102, u_b) + setVar(101, a) + setVar(103, b);
+    gl_FragColor = u_a;
 }
