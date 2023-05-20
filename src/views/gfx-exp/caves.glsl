cd7ceae72d44db6452eaa0c336d87c42d9a8f661
Robin Forbes <rsforbes0@gmail.com>
2023-05-20 04:02:06 +0000

    20 May 2023, 05:02

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -54,7 +54,8 @@ void main() {
     vec4 randomColorStart = _if(!varActive(), _if(isStart, vec4(vec3(rand(gl_FragCoord.xy)), 1.0)));
     
     vec4 texel = pickRandTexel();
-    vec4 transformed = _if(texel.x - getCurrentTexel().x > 0.5, getCurrentTexel() * texel);
+    vec4 transformed = _if(abs(texel.x - getCurrentTexel().x) > 0.5, vec4(getCurrentTexel().xy, texel.x * 0.01, 1.0));
+    transformed += _if(abs(texel.x - getCurrentTexel().x) < 0.5, vec4(getCurrentTexel().xy, texel.x * 0.01, 1.0));
     
     bool invalidStart = getVar(0).w < 1.0 && getVar(0).w > 0.0;
     gl_FragColor = transformed + (newIsStart + randomColorStart) + setVar(100, _if(invalidStart, vec4(1.0, 0.0, 0.0, 1.0)));
