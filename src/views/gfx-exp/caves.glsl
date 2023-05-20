04814cd803dc7da53560ac55b4e9591271ec951c
Robin Forbes <rsforbes0@gmail.com>
2023-05-20 19:42:11 +0000

    20 May 2023, 20:42

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -22,38 +22,25 @@ vec4 setTexel(ivec2 coord, vec4 color) {
 vec4 getScreenTexel(vec4 coord) {
     return texelFetch(u_texture, texelCoord(coord), 0);
 }
-float modI(float a,float b) {
-    float m=a-floor((a+0.5)/b)*b;
-    return floor(m+0.5);
-}
-ivec2 var(int index) {
-    int texelIndex = int(floor(float(index) / 4.0));
-    int componentIndex = int(modI(float(index), 4.0));
-    return ivec2(texelIndex, componentIndex);
+
+float get(int index) {
+    return texelFetch(u_texture, ivec2(index, 0), 0);
 }
 
-float get(ivec2 varI) {
-    vec4 nibble = texelFetch(u_texture, ivec2(varI.x, 0), 0);
-    return nibble[varI.y];
-}
-
-vec4 set(ivec2 varI, float value) {
-    vec4 nibble = texelFetch(u_texture, ivec2(varI.x, 0), 0);
-    nibble[varI.y] = value;
-    return setTexel(ivec2(varI.x, 0), nibble);
+vec4 set(ivec2 varI, vec4 value) {
+    return setTexel(ivec2(varI.x, 0), value);
 }
 
 void main() {
     ivec2 size = textureSize(u_texture, 0);
-    ivec2 vX = var(0);
     
-    vec4 newX = set(vX, float(u_x));
-    float x = get(vX);
+    vec4 newX = set(0, vec4(u_x));
+    vec4 x = get(0);
     vec4 newA = setTexel(ivec2(x, size.y / 2), u_a);
     
     if (texelCoord(gl_FragCoord).y > 0) {
-        gl_FragColor = vec4(vec3(x), 1.0);
+        gl_FragColor = x;
     } else {
-        gl_FragColor = newX + set(var(1), 1.0) + set(var(2), 1.0) + set(var(3), 1.0);
+        gl_FragColor = newX;
     }
 }
