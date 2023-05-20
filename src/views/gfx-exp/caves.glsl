834c27bb4d7232a93ed06dcfdaf87542b089ceee
Robin Forbes <rsforbes0@gmail.com>
2023-05-20 21:43:58 +0000

    20 May 2023, 22:43

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -16,7 +16,7 @@ ivec2 texelCoord(vec4 coord) {
 }
 
 vec4 setTexel(ivec2 coord, vec4 color) {
-    if (coord == texelCoord(gl_FragCoord)) {
+    if(coord == texelCoord(gl_FragCoord)) {
         return color;
     } else {
         return vec4(0.0);
@@ -40,25 +40,25 @@ vec4 debugColor(int index, vec4 value) {
     return gl_FragCoord.x >= findex && gl_FragCoord.x < findex + 8.0 ? value : vec4(0.0);
 }
 
-vec4 render (vec4[arrSize] colors, vec4[arrSize] debugColors, vec4[arrSize] newValues) {
+vec4 render(vec4[arrSize] colors, vec4[arrSize] debugColors, vec4[arrSize] newValues) {
     vec4 color = vec4(0.0);
-    for (int i = 0; i < colors.length(); i++) {
+    for(int i = 0; i < colors.length(); i++) {
         color += colors[i];
     }
-    
+
     vec4 debugColors = vec4(0.0);
-    for (int i = 0; i < debugColors.length(); i++) {
+    for(int i = 0; i < debugColors.length(); i++) {
         debugColors += debugColors[i];
     }
-    
+
     vec4 newValues = vec4(0.0);
-    for (int i = 0; i < newValues.length(); i++) {
+    for(int i = 0; i < newValues.length(); i++) {
         newValues += newValues[i];
     }
-    
-    if (texelCoord(gl_FragCoord).y > 10) {
+
+    if(texelCoord(gl_FragCoord).y > 10) {
         gl_FragColor = color;
-    } else if (texelCoord(gl_FragCoord).y > 0) {
+    } else if(texelCoord(gl_FragCoord).y > 0) {
         gl_FragColor = debugColors;
     } else {
         gl_FragColor = newValues;
@@ -67,16 +67,12 @@ vec4 render (vec4[arrSize] colors, vec4[arrSize] debugColors, vec4[arrSize] newV
 
 void main() {
     ivec2 size = textureSize(u_texture, 0);
-    
+
     vec4 newX = set(200, vec4(u_x));
     vec4 x = get(200);
-    
+
     vec4 started = get(201);
     vec4 newStart = started.w == 0.0 ? set(201, vec4(1.0)) : started;
-    
-    gl_FragColor = render(
-        vec4[arrSize]vec4(0.0)),
-        vec4[arrSize](debugColor(0, x), debugColor(1, started)),
-        vec4[arrSize](newX, newStart)
-    );
+
+    gl_FragColor = render(vec4[arrSize] vec4(0.0)), vec4[arrSize](debugColor(0, x), debugColor(1, started)), vec4[arrSize](newX, newStart));
 }
