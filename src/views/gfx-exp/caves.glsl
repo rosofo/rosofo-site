90aac67564013f1aed9242f3076b97ce11d3a0dc
Robin Forbes <rsforbes0@gmail.com>
2023-05-20 00:13:07 +0000

    20 May 2023, 01:13

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -22,9 +22,13 @@ vec4 getVar(int index) {
     return texelFetch(u_texture, ivec2(index, 0), 0);
 }
 
-vec4 cond(bool p, vec4 v) {
-    return float(p) * v;
-}
+#define COND_TEMPLATE(type) \
+    type cond(bool p, type v) { \
+        return float(p) * v; \
+    }
+
+COND_TEMPLATE(float)
+COND_TEMPLATE(vec4)
 
 bool varActive() {
     ivec2 c = ivec2(gl_FragCoord.x / 3.0f, gl_FragCoord.y / 2.4f);
