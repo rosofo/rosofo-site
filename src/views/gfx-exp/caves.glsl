c9c24d041cc0727856c9e35722b01be9c6917893
Robin Forbes <rsforbes0@gmail.com>
2023-05-20 22:01:05 +0000

    20 May 2023, 23:01

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -38,37 +38,17 @@ vec4 debugColor(int index, vec4 value) {
     return gl_FragCoord.x >= findex && gl_FragCoord.x < findex + 8.0 ? value : vec4(0.0);
 }
 
-#define render(A, B, C) render(A, B, C) \
-void render(vec4[A] colors, vec4[B] debugColors, vec4[C] newValues) { \
-    vec4 color = vec4(0.0f); \
-    for(int i = 0; i < colors.length(); i++) { \
-        color += colors[i]; \
-    } \
-    vec4 debugColor = vec4(0.0); \
-    for(int i = 0; i < debugColors.length(); i++) { \
-        debugColor += debugColors[i]; \
-    } \
-    vec4 newValue = vec4(0.0); \
-    for(int i = 0; i < newValues.length(); i++) { \
-        newValue += newValues[i]; \
-    } \
-    if(texelCoord(gl_FragCoord).y > 10) { \
-        gl_FragColor = color; \
-    } else if(texelCoord(gl_FragCoord).y > 0) { \
-        gl_FragColor = debugColor; \
-    } else { \
-        gl_FragColor = newValue; \
-    } \
-}
-
 void main() {
     ivec2 size = textureSize(u_texture, 0);
 
     vec4 newX = set(200, vec4(u_x));
     vec4 x = get(200);
 
-    vec4 started = get(201);
-    vec4 newStart = started.w == 0.0 ? set(201, vec4(1.0)) : started;
-
-    render(1, 2, 2) (vec4[1](vec4(0.0)), vec4[2](debugColor(0, x), debugColor(1, started)), vec4[2](newX, newStart));
+    if(texelCoord(gl_FragCoord).y > 10) {
+        gl_FragColor = vec4(0.0);
+    } else if(texelCoord(gl_FragCoord).y > 0) {
+        gl_FragColor = debugColor(0, x);
+    } else {
+        gl_FragColor = newX;
+    }
 }
