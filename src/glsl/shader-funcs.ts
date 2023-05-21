5bf85e1f132d8416aff81f70e1eb3d39d9d6cab5
Robin Forbes <rsforbes0@gmail.com>
2023-05-21 23:04:47 +0000

    22 May 2023, 00:04

--- src/glsl/shader-funcs.ts
+++ src/glsl/shader-funcs.ts
@@ -1,11 +1,12 @@
-export type Func = {
+export type Func<P extends Array<string>> = {
   definition: string;
-  caller: (...args: any[]) => string;
+  caller: (...args: P) => string;
 };
 export const defineFuncs = <
   F extends { [P in keyof S]: D },
   S extends string,
-  D extends Func
+  D extends Func<P>,
+  P extends Array<string>
 >(
   funcs: F
 ): { defs: string } & { [P in keyof F]: F[P] } => {
