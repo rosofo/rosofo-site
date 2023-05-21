bb572889510c370fb1e42f88ffe8456821c30b07
Robin Forbes <rsforbes0@gmail.com>
2023-05-21 22:39:12 +0000

    21 May 2023, 23:39

--- src/glsl/shader-funcs.ts
+++ src/glsl/shader-funcs.ts
@@ -2,18 +2,12 @@ export type Func = {
   definition: string;
   caller: (...args: any[]) => string;
 };
-export class ShaderFuncs {
-  funcs: Record<string, Func>;
-  defs: string;
-  callers: Record<string, (...args: any[]) => string>;
-
-  constructor(funcs: Record<string, Func>) {
-    this.funcs = funcs;
-    this.defs = Object.values(funcs)
-      .map((func) => func.definition)
-      .join("\n");
-    this.callers = Object.fromEntries(
-      Object.entries(funcs).map(([name, func]) => [name, func.caller])
-    );
-  }
-}
+export const defineFuncs = (funcs: Record<string, Func>) => {
+  const defs = Object.values(funcs)
+    .map((func) => func.definition)
+    .join("\n");
+  const callers = Object.fromEntries(
+    Object.entries(funcs).map(([name, func]) => [name, func.caller])
+  );
+  return { defs, callers };
+};
