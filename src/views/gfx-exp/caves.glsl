c0a5f27786ca6800e33a89808455fdb51c0580c4
Robin Forbes <rsforbes0@gmail.com>
2023-05-21 01:16:04 +0000

    21 May 2023, 02:16

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -49,8 +49,8 @@ void main() {
     vec4 newStart = started.x == 0.0 ? set(201, vec4(1.0)) : set(201, started);
 
     ivec2 sc = texelCoord(gl_FragCoord);
-    ivec2 mp = u_p2 - u_p1;
-    float dp = dot(vec2(sc - u_p1), vec2(mp));
+    ivec2 mp = (u_p2 - u_p1) / 2;
+    float dp = dot(vec2(sc - u_p1), vec2(u_p2 - u_p1));
     bool isOnLine = dp == 0.0;
     vec4 lineColor = isOnLine ? vec4(0.5, 0.5, 1.0, 1.0) : vec4(0.0);
 
