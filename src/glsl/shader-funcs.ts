c3102485c4691b0bc7efc45a8cdb7d480007644f
Robin Forbes <rsforbes0@gmail.com>
2023-05-21 22:58:20 +0000

    21 May 2023, 23:58

--- src/glsl/shader-funcs.ts
+++ src/glsl/shader-funcs.ts
@@ -2,7 +2,13 @@ export type Func = {
   definition: string;
   caller: (...args: any[]) => string;
 };
-export const defineFuncs = (funcs: Record<string, Func>) => {
+export const defineFuncs = <
+  F extends { [P in keyof S]: D },
+  S extends string,
+  D extends Func
+>(
+  funcs: F
+): { defs: string } & { [P in keyof S]: F[P] } => {
   const defs = Object.values(funcs)
     .map((func) => func.definition)
     .join("\n");
