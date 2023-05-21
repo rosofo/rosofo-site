8dc877153e7c8cee9c360f3ea77bae1252fefcfe
Robin Forbes <rsforbes0@gmail.com>
2023-05-21 19:25:04 +0000

    21 May 2023, 20:25

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -63,7 +63,7 @@ void main() {
     ivec2 sc = texelCoord(gl_FragCoord);
     mat2 qtTurn = mat2(cos(0.5 * PI), -sin(0.5 * PI), sin(0.5 * PI), cos(0.5 * PI));
     vec3 lineVec = vec3(vec2(u_p1) - vec2(p2), 0.0);
-    vec4 line = normalize(vec2(u_p1) - vec2(sc)) == -normalize(vec2(p2) - vec2(sc)) ? vec4(1.0) : vec4(0.0);
+    vec4 line = (normalize(vec2(u_p1) - vec2(sc)) - normalize(vec2(p2) - vec2(sc)) < 0.01 ? vec4(1.0)) : vec4(0.0);
 
     if(texelCoord(gl_FragCoord).y > 10) {
         gl_FragColor = line + setTexel(u_p1, vec4(0.09f, 0.06f, 0.78f, 1.0f)) + setTexel(p2, vec4(0.09f, 0.06f, 0.78f, 1.0f));
