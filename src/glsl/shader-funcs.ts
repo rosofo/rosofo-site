3296d27d74a5b6b96cc75bd358e666bcdeceb610
Robin Forbes <rsforbes0@gmail.com>
2023-05-21 23:21:04 +0000

    22 May 2023, 00:21

--- src/glsl/shader-funcs.ts
+++ src/glsl/shader-funcs.ts
@@ -1,19 +1,19 @@
-export interface Func<P extends Array<string>> {
+export interface Func<P extends Array<A>, A> {
   definition: string;
   (...args: P): string;
 }
-export const defineFuncs = <P extends Array<string>>(
-  funcs: Func<P>[]
+export const defineFuncs = <P extends Array<A>, A>(
+  funcs: Func<P, A>[]
 ): string => {
   return Object.values(funcs)
     .map((func) => func.definition)
     .join("\n");
 };
 
-export const func = <P extends Array<string>>(
+export const func = <P extends Array<A>, A>(
   def: string,
   caller: (...args: P) => string
-): Func<P> => {
+): Func<P, A> => {
   const func: any = { ...caller };
   func.definition = def;
   return func;
