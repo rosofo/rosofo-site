e2f124fa649b85e94bc2eef77ba92119b8bcdab7
Robin Forbes <rsforbes0@gmail.com>
2023-05-21 01:04:41 +0000

    21 May 2023, 02:04

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -48,11 +48,12 @@ void main() {
     vec4 started = get(201);
     vec4 newStart = started.x == 0.0 ? set(201, vec4(1.0)) : set(201, started);
 
-    vec4 fill = started.x == 0.0 ? vec4(0.0, 0.0, 0.0, 1.0) : vec4(0.0);
-
     ivec2 sc = texelCoord(gl_FragCoord);
-    bool isOnLine = dot(vec2(sc - u_p1), vec2(u_p2 - u_p1)) == 0.0;
+    float dp = dot(vec2(sc - u_p1), vec2(u_p2 - u_p1));
+    bool isOnLine = dp == 0.0;
     vec4 lineColor = isOnLine ? vec4(0.5, 0.5, 1.0, 1.0) : vec4(0.0);
+
+    vec4 fill = vec4(dp) / float(size.x);
 
     if(texelCoord(gl_FragCoord).y > 10) {
         gl_FragColor = lineColor + fill;
