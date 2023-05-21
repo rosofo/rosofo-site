47684b0a381b0bbc165e3845773bf403ffa5e548
Robin Forbes <rsforbes0@gmail.com>
2023-05-21 09:46:29 +0000

    21 May 2023, 10:46

--- src/views/gfx-exp/caves.glsl
+++ src/views/gfx-exp/caves.glsl
@@ -61,7 +61,7 @@ void main() {
     if(texelCoord(gl_FragCoord).y > 10) {
         gl_FragColor = fill + setTexel(p2, vec4(0.09f, 0.06f, 0.78f, 1.0f));
     } else if(texelCoord(gl_FragCoord).y > 0) {
-        gl_FragColor = debugColor(0, x) + debugColor(1, started) + debugColor(2, vec4(vec2(p2.xy) / 400.0, 0.0, 1.0));
+        gl_FragColor = debugColor(0, x) + debugColor(1, started) + debugColor(2, vec4(p2.x));
     } else {
         gl_FragColor = newX + newStart + newP2;
     }
