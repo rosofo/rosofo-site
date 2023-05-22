baa66e3cf855a5d5e79a18173d73cc955b3f6472
Robin Forbes <rsforbes0@gmail.com>
2023-05-22 22:36:32 +0000

    22 May 2023, 23:36

--- src/glsl/shader-funcs.ts
+++ src/glsl/shader-funcs.ts
@@ -1,16 +1,16 @@
-export type Func<P extends A[], A> = {
+export type Func<P> = {
   def: string;
-  caller: (...args: P) => string;
+  caller: (...args: P[]) => string;
 };
 
-export const func = <P extends A[], A>(
+export const func = <P>(
   def: string,
-  caller: (...args: P) => string
-): Func<P, A> => ({
+  caller: (...args: P[]) => string
+): Func<P> => ({
   def,
   caller,
 });
 
-export const defineFuncs = <P extends A[], A>(funcs: Func<P, A>[]) => {
+export const defineFuncs = <P>(funcs: Func<P>[]) => {
   return funcs.map((f) => f.def).join("\n");
 };
