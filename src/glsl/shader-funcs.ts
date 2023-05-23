baf27078170b46935e90c68ecdda3780ddfb85e0
Robin Forbes <rsforbes0@gmail.com>
2023-05-23 09:59:39 +0000

    23 May 2023, 10:59

--- src/glsl/shader-funcs.ts
+++ src/glsl/shader-funcs.ts
@@ -1,22 +1,20 @@
 export type Func<T, U = unknown, V = unknown> = {
   def: string;
-} & (
+} & F<T, U, V>;
+
+type F<T, U = unknown, V = unknown> =
   | {
       (a: T): string;
     }
   | { (a: T, b: U): string }
-  | { (a: T, b: U, c: V): string }
-);
+  | { (a: T, b: U, c: V): string };
 
 export const func = <T, U = unknown, V = unknown>(
   def: string,
-  caller: Omit<Func<T, U, V>, "def">
+  caller: F<T, U, V>
 ): Func<T, U, V> => {
-  const f = ((...args: any[]) => {
-    return (caller as any)(...args);
-  }) as any;
-  f.def = def;
-  return f;
+  const f = caller.bind({ def });
+  return f as Func<T, U, V>;
 };
 export const defineFuncs = (funcs: Func<any>[]) => {
   return funcs.map((f) => f.def).join("\n");
