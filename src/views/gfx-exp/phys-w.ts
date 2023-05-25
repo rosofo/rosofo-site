5d727f6af2f65336969e47a85a180343d6869784
Robin Forbes <rsforbes0@gmail.com>
2023-05-25 02:02:51 +0000

    25 May 2023, 03:02

--- src/views/gfx-exp/phys-w.ts
+++ src/views/gfx-exp/phys-w.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from "lit";
+import { LitElement, html, css, PropertyValueMap } from "lit";
 import { customElement } from "lit/decorators.js";
 import "../../components/shader-plane";
 import * as THREE from "three";
@@ -16,9 +16,22 @@ export class PhysW extends LitElement {
 
   ref = createRef();
 
+  protected firstUpdated(
+    _changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>
+  ): void {
+    start(this.ref.value!);
+  }
+
   render() {
     return html`
       <canvas width=${WIDTH} height=${HEIGHT} ${ref(this.ref)}></canvas>
     `;
   }
 }
+
+const start = (elem: Element) => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.OrthographicCamera();
+  const renderer = new THREE.WebGLRenderer({ canvas: elem });
+  renderer.setSize(WIDTH, HEIGHT);
+};
